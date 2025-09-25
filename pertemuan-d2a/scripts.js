59eddbab6df66b4738142eb947fb662eb07c88d3
azki0102-afk <azki0102@student.abudzar.sch.id>
2025-09-25 10:25:17 +0700

    tugas-17.html

---
+++ pertemuan-d2a/scripts.js
@@ -0,0 +1,26 @@
+massage = "gacoan"
+alert(massage)
+
+let kresek;
+kresek = "tek-tek"
+console . logI(kresek)
+
+let umur = 20; //  sebuah angka/numerik
+
+let name = "azki", age = 15, instudent = trust
+
+let name1 = "azki",
+    age = 15,
+    instudent = true;
+
+ let name2 =  "akia"
+ let age = 23;
+ let  instudent = true;
+ console log (name2, age2,instudent2);
+
+ abu = 'white' ;
+ console.log(abu); "white"
+
+ let $na9sde9 = "azki"
+ let keranjangbelanja = " metode camel case"
+
---
+++ pertemuan-tiga/script.js
@@ -0,0 +1,38 @@
+// alert("sudah terintragasi");
+console.log("test")
+
+let kresek1 = "bakwan";
+let kresek2 = 100;
+let kresek3 =20;
+
+let tkresekhasil = kresek1+ kresek2
+
+console.log(kresek1); 'bakwan'
+console.log(kresek2); 100
+console.log(kresekhasil); 120
+
+let umur = 20;
+console = (umur);
+
+umur = "dua puluh";
+console.log(umur); 'dua puluh'
+
+
+let angkabeasar = 21212121345335484n;
+
+console.log
+
+let contohsatu = "diampitoleh petik atas 2"; " + kreseksatu + " ini tambahn tulisan"
+let consoledua = "diampit oleh pentik "
+let consoletiga = "mengunakan backpit $(kresek1) ini tambahna tulisan':
+
+console.log (contohsatu + kresek1); 'diapit oleh petik 2 bakwan'
+
+let hidup = 1:                                                   
+let mati = 0;
+
+let gayung = "air"
+console.log(type gayung)  'string';
+
+let age = promt("berapakah umur anda" . 100) 
+alert("umur anda adalah" + age);
